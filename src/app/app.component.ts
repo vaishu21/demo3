eae4fd6e3feb8c9e3493dca7a0a04d3e9fbfbd5b
vaishu21 <jain99.vaishali@gmail.com>
2019-02-10 09:17:39 -0800

    angular first code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pass :string;
+  myUsername:string;
+
+   testdemo3(){
+   if(this.pass=="vaishu" && this.myUsername=="vaishu123"){
+     alert("Hi vaishu");
+   }
+   else{
+     alert("fuck off");
+   }
+
+   }
+
+  
   title = 'demo3';
 }
